323c0c59957db219250bb6088f9c5a0347cfe3e4
sampreeth1998 <sampreeth1998@live.com>
2020-04-01 22:26:26 +0530

    button41

--- InvoiceDisplay.aspx.designer.cs
+++ InvoiceDisplay.aspx.designer.cs
@@ -706,13 +706,13 @@ namespace FinalYearProject {
         protected global::System.Web.UI.WebControls.TextBox TextBox10;
         
         /// <summary>
-        /// Button24 control.
+        /// Button41 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button24;
+        protected global::System.Web.UI.WebControls.Button Button41;
         
         /// <summary>
         /// Button3 control.
