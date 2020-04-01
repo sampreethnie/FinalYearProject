f0d2e03a3a8d47589512d2724d5eb95a79aab939
sampreeth1998 <sampreeth1998@live.com>
2020-04-01 23:31:00 +0530

    btn85

--- InvoiceDisplay.aspx.designer.cs
+++ InvoiceDisplay.aspx.designer.cs
@@ -706,13 +706,13 @@ namespace FinalYearProject {
         protected global::System.Web.UI.WebControls.TextBox TextBox10;
         
         /// <summary>
-        /// Button90 control.
+        /// Button85 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button90;
+        protected global::System.Web.UI.WebControls.Button Button85;
         
         /// <summary>
         /// Button3 control.
