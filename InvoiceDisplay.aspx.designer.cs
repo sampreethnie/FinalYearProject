cc39e80a99ae027603000c1a657dcb463fae32bb
sampreeth1998 <sampreeth1998@live.com>
2020-04-01 22:17:25 +0530

    Revert "button25"
    This reverts commit f13c1520622c721a164fbd5025f3a35f702af6e1.

--- InvoiceDisplay.aspx.designer.cs
+++ InvoiceDisplay.aspx.designer.cs
@@ -706,13 +706,13 @@ namespace FinalYearProject {
         protected global::System.Web.UI.WebControls.TextBox TextBox10;
         
         /// <summary>
-        /// Button25 control.
+        /// Button24 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button25;
+        protected global::System.Web.UI.WebControls.Button Button24;
         
         /// <summary>
         /// Button3 control.
